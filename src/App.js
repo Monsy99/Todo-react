28a7ca9ce5e894f44c580d59a1214320bb6dda9d
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-09-18 22:40:13 +0200

    changed tasks and doneTasksHidden to useState, created toggleTaskDone function

--- src/App.js
+++ src/App.js
@@ -1,43 +1,64 @@
-import React from 'react';
+import React, { useState } from "react";
 import Form from "./Form";
-import Tasks from './Tasks';
-import Buttons from './Buttons';
-import Group from './Group';
-import './style.css'
-
-const tasks = [
-    {
-        id:1,
-        content:"zjeść ciastko",
-        done:true,
-    },
-    {
-        id:2,
-        content:"wypić herbatę",
-        done:false,
-    }
-]
-const doneTasksHidden = false;
+import Tasks from "./Tasks";
+import Buttons from "./Buttons";
+import Group from "./Group";
+import "./style.css";
 
 function App() {
-    return (
-        <main className="main">
-            <h1 className="main__header">Lista zadań</h1>
-            
-            <Group 
-            title="Dodaj nowe zadanie"
-            >
-                <Form/>
-            </Group>
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      content: "zjeść ciastko",
+      done: true,
+    },
+    {
+      id: 2,
+      content: "wypić herbatę",
+      done: false,
+    },
+  ]);
 
-            <Group 
-            title="Lista zadań" 
-            additionalElements={<Buttons tasks={tasks} doneTasksHidden={doneTasksHidden}/>}
-            >
-                <Tasks tasks={tasks} doneTasksHidden={doneTasksHidden} />
-            </Group>
-        </main>
-    );
+  const [doneTasksHidden, setDoneTasksHidden] = useState(false);
+
+  const toggleTaskDone = (id) => {
+    return tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          task,
+          done: !task.done,
+        };
+      }
+      return task;
+    });
+  };
+
+  return (
+    <main className="main">
+      <h1 className="main__header">Lista zadań</h1>
+
+      <Group title="Dodaj nowe zadanie">
+        <Form />
+      </Group>
+
+      <Group
+        title="Lista zadań"
+        additionalElements={
+          <Buttons
+            tasks={tasks}
+            doneTasksHidden={doneTasksHidden}
+            setDoneTasksHidden={setDoneTasksHidden}
+          />
+        }
+      >
+        <Tasks
+          toggleTaskDone={toggleTaskDone}
+          tasks={tasks}
+          doneTasksHidden={doneTasksHidden}
+        />
+      </Group>
+    </main>
+  );
 }
 
-export default App; 
+export default App;
