8c7403e346bfe0a9bb55f6da00a4dc03e40e3d3d
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-09-18 22:41:13 +0200

    fixed class instead of className issue

--- src/Group/index.js
+++ src/Group/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 
 const Group = (props) => (
-        <div className="group">
-            <div class="group__wrapper">
-                <h2 className="group__subHeader">{props.title}</h2>
-                {props.additionalElements}
-            </div>
-            <hr></hr>
-            {props.children}
-        </div>
-)
+  <div className="group">
+    <div className="group__wrapper">
+      <h2 className="group__subHeader">{props.title}</h2>
+      {props.additionalElements}
+    </div>
+    <hr></hr>
+    {props.children}
+  </div>
+);
 
 export default Group;
