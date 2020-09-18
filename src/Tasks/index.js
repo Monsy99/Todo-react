fc9a4fcfc861457e08cf337eb2c51fb07f03cd9e
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-09-18 22:42:17 +0200

    added key to each child of Tasks and  added onclick interaction to done button

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,20 +1,25 @@
 import React from "react";
 import "./style.css";
 
-
-const Tasks = (props) => ( 
+const Tasks = ({ tasks, doneTasksHidden, toggleTaskDone }) => {
+  return (
     <ul className="tasks">
-        {props.tasks.map(( task => 
-            <li className={`task${props.doneTasksHidden && task.done ? " task--hidden" : ""}${task.done ? " task--done" : ""}`}>
-                <button className={`task__doneButton`}>✔</button>
-                <p className={`task__content`}>
-                    {task.content}
-                </p>
-                <button className={`task__deleteButton`}>✖</button>
-            </li>
-        ))}
+      {tasks.map((task) => (
+        <li
+          key={task.id}
+          className={`task
+          ${doneTasksHidden && task.done ? " task--hidden" : ""}
+          ${task.done ? " task--done" : ""}`}
+        >
+          <button className={`task__doneButton`} onClick={() => toggleTaskDone}>
+            ✔
+          </button>
+          <p className={`task__content`}>{task.content}</p>
+          <button className={`task__deleteButton`}>✖</button>
+        </li>
+      ))}
     </ul>
-)
-
+  );
+};
 
 export default Tasks;
